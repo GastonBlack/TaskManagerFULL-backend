cbd8f10c6099fe6c5bf649a619ae04b67b9ff11f
GastonBlack <gaston.galeano.r@gmail.com>
2025-11-14 21:06:53 -0300

    Fix Render deploy

--- Program.cs
+++ Program.cs
@@ -6,15 +6,25 @@ using ToDoAppAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Render: bindear puerto dinámico
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// JSON Settings
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
     options.SerializerOptions.WriteIndented = true;
 });
+
+// PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Controllers y Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -25,28 +35,31 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend", builder =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        builder
-            .WithOrigins("https://taskmanager-full.vercel.app/")
+        policy
+            .WithOrigins("https://taskmanager-full.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
     });
 });
 
-
 builder.Services.AddScoped<TaskService>();
+
 var app = builder.Build();
 
-// APLICAR CORS.
+// Produccion
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// CORS
 app.UseCors("AllowFrontend");
+
+// Controllers
 app.MapControllers();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 app.Run();
